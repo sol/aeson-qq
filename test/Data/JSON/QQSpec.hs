305ffc4250702c74895559f698254232f06dc276
Phil de Joux <philderbeast@gmail.com>
2023-05-20 20:43:51 -0400

    Satisfy -Wincomplete-uni-patterns.

--- test/Data/JSON/QQSpec.hs
+++ test/Data/JSON/QQSpec.hs
@@ -12,39 +12,45 @@ spec :: Spec
 spec = do
   describe "parsedJson" $ do
     it "parses JSON" $ do
-      let Right value = parsedJson "{foo: 23}"
-      value `shouldBe` JsonObject [(HashStringKey "foo", JsonNumber 23)]
+      let value = parsedJson "{foo: 23}"
+      value `shouldSatisfy` either
+        (const False)
+        (== JsonObject [(HashStringKey "foo", JsonNumber 23)])
 
     it "parses decimal numbers" $ do
-      let Right value = parsedJson "{foo: 5.97}"
-      value `shouldBe` JsonObject [(HashStringKey "foo", JsonNumber 5.97)]
+      let value = parsedJson "{foo: 5.97}"
+      value `shouldSatisfy` either
+        (const False)
+        (== JsonObject [(HashStringKey "foo", JsonNumber 5.97)])
 
     context "empty objects" $ do
       it "parses empty objects (regression test)" $ do
-        let Right value = parsedJson "{}"
-        value `shouldBe` JsonObject []
+        let value = parsedJson "{}"
+        value `shouldSatisfy` either (const False) (== JsonObject [])
 
       it "parses empty objects that include whitespace (regression test)" $ do
-        let Right value = parsedJson "{ }"
-        value `shouldBe` JsonObject []
+        let value = parsedJson "{ }"
+        value `shouldSatisfy` either (const False) (== JsonObject [])
 
       it "parses empty objects that include newlines (regression test)" $ do
-        let Right value = parsedJson "{\n}"
-        value `shouldBe` JsonObject []
+        let value = parsedJson "{\n}"
+        value `shouldSatisfy` either (const False) (== JsonObject [])
 
     context "empty arrays" $ do
       it "parses empty arrays" $ do
-        let Right value = parsedJson "[   ]"
-        value `shouldBe` JsonArray []
+        let value = parsedJson "[   ]"
+        value `shouldSatisfy` either (const False) (== JsonArray [])
 
       it "parses empty arrays that include whitespace (regression test)" $ do
-        let Right value = parsedJson "[   ]"
-        value `shouldBe` JsonArray []
+        let value = parsedJson "[   ]"
+        value `shouldSatisfy` either (const False) (== JsonArray [])
 
       it "parses empty objects that include newlines (regression test)" $ do
-        let Right value = parsedJson "[\n]"
-        value `shouldBe` JsonArray []
+        let value = parsedJson "[\n]"
+        value `shouldSatisfy` either (const False) (== JsonArray [])
 
     it "fails on excess input" $ do
-      let Left err = parsedJson "{foo: 23} some excess input"
-      show err `shouldBe` "\"txt\" (line 1, column 11):\nunexpected 's'\nexpecting space or end of input"
+      let value = parsedJson "{foo: 23} some excess input"
+      value `shouldSatisfy` either
+        ((== "\"txt\" (line 1, column 11):\nunexpected 's'\nexpecting space or end of input") . show)
+        (const False)
